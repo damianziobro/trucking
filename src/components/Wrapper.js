242c5a4c180129396c8608efc46942605a90786e
Damian Ziobro <devdamianziobro@gmail.com>
2018-07-01 11:55:34 +0200

    Created Wrapper styled component

---
+++ src/components/Wrapper.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  max-width: 1200px;
+  padding: 1em;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Wrapper;
