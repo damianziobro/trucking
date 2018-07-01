798faa24aa5d615e29a2549af9ecba303de32982
Damian Ziobro <devdamianziobro@gmail.com>
2018-07-01 11:58:00 +0200

    Added styles for HeroSection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import BlogPost from '../components/BlogPost';
 import Offer from '../components/Offer';
+import Wrapper from '../components/Wrapper';
+
+const HeroSection = styled.section`
+  background-color: #122636;
+
+  h2 {
+    font-weight: 800;
+    font-size: 7.2em;
+    text-transform: uppercase;
+    color: #ffffff;
+    width: 50%;
+    align-self: flex-start;
+  }
+
+  h2::before {
+    content: '';
+    display: block;
+    width: 70px;
+    height: 7px;
+    background-color: #fdc300;
+  }
+`;
+
+const BlogPosts = styled.ul`
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  list-style-type: none;
+`;
+
+const ViewDetailsBtn = styled.button`
+  margin: 50px 0;
+  width: 220px;
+  height: 50px;
+  border: 0;
+  border-radius: 25px;
+  background-color: #fdc300;
+  text-transform: uppercase;
+  font-weight: 700;
+  color: #ffffff;
+  font-size: 1.3em;
+  cursor: pointer;
+  transition: 0.3s;
+  opacity: 1;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
 
 const IndexPage = ({
   data: {
@@ -11,23 +63,31 @@ const IndexPage = ({
 }) => {
   return (
     <div>
+      <HeroSection>
+        <Wrapper>
+          <h2>Strongest distribution network</h2>
+          <BlogPosts>
+            {blogPostsData.map(blogPostData => (
+              <BlogPost
+                key={blogPostData.node.id}
+                blogPostData={blogPostData}
+              />
+            ))}
+          </BlogPosts>
+          <ViewDetailsBtn>View details</ViewDetailsBtn>
+        </Wrapper>
+      </HeroSection>
+
       <section>
-        <h2>Strongest distribution network</h2>
-        <ul>
-          {blogPostsData.map(blogPostData => (
-            <BlogPost key={blogPostData.node.id} blogPostData={blogPostData} />
-          ))}
-        </ul>
-        <button>View details</button>
-      </section>
-      <section>
-        <h2>What we offer</h2>
-        <p>Tailored logistic services</p>
-        <ul>
-          {offersData.map(offerData => (
-            <Offer key={offerData.node.id} offerData={offerData} />
-          ))}
-        </ul>
+        <Wrapper>
+          <h2>What we offer</h2>
+          <p>Tailored logistic services</p>
+          <ul>
+            {offersData.map(offerData => (
+              <Offer key={offerData.node.id} offerData={offerData} />
+            ))}
+          </ul>
+        </Wrapper>
       </section>
     </div>
   );
