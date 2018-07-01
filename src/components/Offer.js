183a0dfa26b000286bb5e8390c8d4363d9699b68
Damian Ziobro <devdamianziobro@gmail.com>
2018-07-01 13:15:54 +0200

    Added styles for Offer component

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,5 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const OfferItem = styled.li`
+  margin-bottom: 30px;
+  padding: 35px 35px 35px 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 185px;
+  width: calc(50% - 60px);
+  background-color: #fcfcfc;
+`;
+
+const OfferIcon = styled.img`
+  margin: 30px;
+  height: 70px;
+`;
+
+const OfferTitle = styled.h3`
+  font-weight: 700;
+  font-size: 1.8em;
+  color: #333333;
+`;
+
+const OfferText = styled.p`
+  font-size: 1.3em;
+  color: #777777;
+  font-family: 'Open Sans';
+`;
 
 const Offer = ({
   offerData: {
@@ -13,11 +42,13 @@ const Offer = ({
   },
 }) => {
   return (
-    <li>
-      <img src={url} />
-      <h3>{title}</h3>
-      <p>{text}</p>
-    </li>
+    <OfferItem>
+      <OfferIcon src={url} />
+      <div>
+        <OfferTitle>{title}</OfferTitle>
+        <OfferText>{text}</OfferText>
+      </div>
+    </OfferItem>
   );
 };
 
