f6c179de1d1a82d5a4a276df389a06553f0be4b7
Damian Ziobro <devdamianziobro@gmail.com>
2018-07-01 05:23:27 +0200

    Configured gatsby-source-contentful to get data from contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Trucking',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: '2z4g2bzv111r',
+        // You need a accessToken here to run the project. I don't publish it to public repo because of security reasons.
+        accessToken: '',
+      },
+    },
+  ],
+};
